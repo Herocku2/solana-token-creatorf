5039f61e6ad8e5619f8f9d44afa051443e36fed5
Giovanoti1 Cubillos <macmini@Mac-mini-de-Giovanoti1.local>
2025-07-15 16:41:26 -0500

    Fix hydration error by adding ClientOnly wrapper component

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,15 @@
 import { Montserrat } from "next/font/google";
 import "./globals.css";
 import AppWalletProvider from "@/provider/AppWalletProvider";
+import ClientOnly from "@/components/ClientOnly";
 import { Toaster } from "sonner";
+
 const montserrat = Montserrat({
   weight: ["400", "500", "600", "700"],
   subsets: ["latin"],
   variable: "--font-montserrat",
 });
+
 export const metadata = {
   title: "Solana Token Creator",
   description: "Create your own Solana token",
@@ -16,10 +19,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${montserrat.variable}`}>
-        <AppWalletProvider>
-          <Toaster position="top-center" />
-          {children}
-        </AppWalletProvider>
+        <ClientOnly>
+          <AppWalletProvider>
+            <Toaster position="top-center" />
+            {children}
+          </AppWalletProvider>
+        </ClientOnly>
       </body>
     </html>
   );
